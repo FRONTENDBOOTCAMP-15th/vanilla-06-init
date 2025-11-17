1bd964e4dc7c93e03d1a42495f63e043ebcfcc2c
txwjd <wkdtxwjd@gmail.com>
2025-11-18 00:35:57 +0900

    [feat] 구독 컴포넌트

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,5 @@ import './styles/main.css';
 import './components/Header.ts';
 import './components/Footer.ts';
 import './components/Navigation.ts';
+import "./components/SubscribeButtonComponent.ts";
+

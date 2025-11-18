8bd728e2feea251af397c5f886173624b814b673
moonhyukjin <moonhyukjin@naver.com>
2025-11-18 15:24:13 +0900

    [feat] search page api and drawer page ui

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,4 +1,25 @@
-export interface User {
+export interface UserItem {
+  _id: number;
+  email: string;
   name: string;
+  phone: string;
+  address: string;
+  type: 'user' | 'seller' | 'admin';
+  loginType: 'email' | 'kakao';
   image: string;
+  createdAt: string;
+  updatedAt: string;
+  extra: {
+    birthday: string;
+    membershipClass: string;
+    address: [
+      {
+        id: number;
+        name: string;
+        value: string;
+      },
+    ];
+    statusMsg: string | null;
+    keyword: string[] | null;
+  };
 }
